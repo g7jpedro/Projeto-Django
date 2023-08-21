6dfca92088555674c032bbc895a9893a6bf28522
João Pedro <g7jpedro@gmail.com>
2023-08-21 18:59:29 -0400

    Finalização do HEAD do site

--- receitas/views.py
+++ receitas/views.py
@@ -2,12 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'receitas/home.html')
+    return render(request, 'receitas/paginas/home.html')
 
-
-def sobre(request):
-    return render(request, 'receitas/sobre.html')
-
-
-def contato(request):
-    return render(request, 'receitas/contato.html')
