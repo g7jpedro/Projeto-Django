720d14077568c1acafcf1e67f6ddbfdb5cd47345
João Pedro <g7jpedro@gmail.com>
2023-08-28 17:41:25 -0400

    Adicionando informações nos templates com faker

--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from uteis.receitas.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'receitas/paginas/home.html')
+    return render(request, 'receitas/paginas/home.html', context = {
+        'receitas': [make_recipe() for _ in range(10)],
+    })
 
 
 def receitas(request, id):
-    return render(request, 'receitas/paginas/receitas-view.html')
+    return render(request, 'receitas/paginas/receitas-view.html', context= {
+        'receita': make_recipe(),
+    })
