58afb2941bde915282492515d89af0d131c40284
João Pedro <g7jpedro@gmail.com>
2023-08-31 15:02:03 -0400

    Criando Models

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categoria(models.Model):
+    categoria = models.CharField('Categoria', max_length=100)
+
+class Receita(models.Model):
+    titulo = models.CharField('Titulo', max_length=50)
+    descricao = models.CharField('Descrição', max_length=160)
+    models.SlugField()
+    tempo_preparacao = models.IntegerField('Tempo de preparação')
+    tempo_preparacao_unidade = models.CharField('Tempo preparação unidade', max_length=65)
+    porcoes = models.IntegerField('Porções')
+    porcoes_unidade = models.CharField('Porções Unidade', max_length=65)
+    preparacao =  models.TextField('Preparação')
+    preparacao_is_html = models;models.BooleanField('Preparação is hmtl', default=False)
+    criacao = models.DateTimeField('Criação', auto_now_add=True)
+    modificacao = models.DateTimeField('Modificação', auto_now=True)
+    is_publicado = models.BooleanField('Públicado ?', default=False)
+    imagem = models.ImageField(upload_to='receitas/imageens/%Y/%m/%d/')
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
