d5ec47301bfabe602f3f08459ee737ef76f616e7
João Pedro <g7jpedro@gmail.com>
2023-09-08 18:34:30 -0400

    Resolvendo problemas na migrations

--- receitas/models.py
+++ receitas/models.py
@@ -13,10 +13,18 @@ class Receita(models.Model):
     porcoes = models.IntegerField('Porções')
     porcoes_unidade = models.CharField('Porções Unidade', max_length=65)
     preparacao =  models.TextField('Preparação')
-    preparacao_is_html = models;models.BooleanField('Preparação is hmtl', default=False)
+    preparacao_is_html = models.BooleanField('Preparação is hmtl', default=False)
     criacao = models.DateTimeField('Criação', auto_now_add=True)
     modificacao = models.DateTimeField('Modificação', auto_now=True)
     is_publicado = models.BooleanField('Públicado ?', default=False)
-    imagem = models.ImageField(upload_to='receitas/imageens/%Y/%m/%d/')
+    imagem = models.ImageField(upload_to='receitas/imagens/%Y/%m/%d/')
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
     autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = 'Receita'
+        verbose_name_plural = 'Receitas'
+
+    
+    def __str__(self):
+        return self.titulo[:10]
